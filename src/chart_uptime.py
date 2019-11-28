9b2dbf5e3a5428563fedb7aa6867225904c2077c
DoggoMan <robspinoglio@gmail.com>
2019-11-29 02:26:42 +1000

    graphing up to 5 player's deacs FOR/AGAINST for a given game

--- src/chart_uptime.py
+++ src/chart_uptime.py
@@ -1,36 +1,107 @@
+# file imports
 import functions
 import parse
 import event_types
+
+# module imports
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 base_filename = " - Space Marines 5.tdf"
 base_filepath = "./data/raw/"
 
-this_filename = "%s%s" % (2004, base_filename)
-this_filepath = "%s%s" % (base_filepath, 20191125)
+# use these two variables to target games with specific dates and game IDs
+this_filename = 2004
+this_filepath = 20191125
+
+# easy constant for manipulating number of plots returned
+max_graphs = 5
 
 # filename = "1216 - Space Marines 5.tdf"
 # filepath = "./data/test/"
 # filename = "2004 - Space Marines 5.tdf"
 # filepath = "./data/raw/20191125"
 
-# pd.set_option('display.max_rows', 10)
+# concat the template and specific variables together
+filename = "%s%s" % (this_filename, base_filename)
+filepath = "%s%s" % (base_filepath, this_filepath)
 
-(data, labels) = parse.read_and_return_sections(this_filename, this_filepath)
+pd.set_option('display.max_rows', 50)
+
+(data, labels) = parse.read_and_return_sections(filename, filepath)
 print(labels)
 
 print(data[2])
 print('\n')
-for index, row in data[2].iterrows():
+
+players = [(index, row) for index, row in data[2].iterrows() if row['position']
+           != 'Referee' and row['position'] != 'Field']
+print("identified %s players from %s entity-start's" %
+      (len(players), len(data[2])))
+
+# fig, subplots = plt.subplots(len(data[2]), 1)
+fig, subplots = plt.subplots(max_graphs, 1)
+graph_i = 0
+
+# for index, row in data[2].iterrows():
+for index, row in players:
     # skip non-players
     if row['position'] == 'Referee' or row['position'] == 'Field':
         continue
 
-    # retrieves all events relating to this player
-    events = [e for e_index, e in data[3].iterrows() if row['id']
-              in e['varies']]
-    self_events = [e for e_index, e in data[3].iterrows() if
-                   e['varies'].find(row['id']) == 0]
+    player_team = data[1][data[1]['index'] == row['team']]['desc']
+    player_team_str = player_team.tolist()[0].split(' ')[0]
+
+    # retrieve all events relating to this player
+    events = data[3][data[3]['varies'].str.contains(row['id'])]
+
+    # events['time_str'] = events['time'].apply(
+    #     lambda x: functions.millis_to_msr(x, 'human'))
+
+    self_events = data[3][data[3]['initiator'].str.contains(row['id'])]
+    else_events = data[3][data[3]['target'].str.contains(row['id'])]
+
+    deac_types = [
+        "0206",
+        "0306",
+        # enemy nuke?? not sure how to find these easily
+    ]
+
+    self_deacs = else_events[else_events['type'].isin(deac_types)]
+    enemy_deacs = self_events[self_events['type'].isin(deac_types)]
+
     print("[%s / %s] \t%s" % (len(self_events), len(events), row['desc']))
 
-    
+    if graph_i < max_graphs:
+        # print(events[['varies']])
+        # print(self_deacs)
+        print(f"Creating graph #{graph_i} for player {row['desc']}")
+
+        x = [int(deac['time']) for index, deac in self_deacs.iterrows()]
+        y = np.full((len(x), 1), -1)
+
+        x2 = [int(deac['time']) for index, deac in enemy_deacs.iterrows()]
+        y2 = np.full((len(x2), 1), 1)
+
+        # use the graph index variable
+        plot = subplots[graph_i]
+        graph_i = graph_i + 1
+
+        plot.set(xlim=(0, 1000*60*15), ylim=(-3, 3))
+
+        # plot.plot(x, y, x2, y2, color='black')
+        plot.scatter(x, y, color='red', marker='o', s=2)
+        plot.scatter(x2, y2, color='green', marker='o', s=2)
+
+        plot.title.set_text('%s - %s %s' %
+                            (row['desc'], player_team_str, row['position']))
+
+    # score graph ?
+    # line graph of player's score as it changed throughout the game?
+    # plot.set(xlim=(0, 1000*60*15), ylim=(0, 15000))
+
+print('Rendering graphs...')
+
+fig.subplots_adjust(hspace=1.0)
+plt.show()
